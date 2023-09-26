bb4de87fa44e01fd725fe849fa2180c6f2717ff1
NasywaHanifa128@gmail.com <fatmafatimatuzzahra66@gmail.com>
2023-09-26 14:52:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/data_guru_fatma/fatma_data_siswa.kt
@@ -0,0 +1,11 @@
+package com.example.data_guru_fatma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fatma_data_siswa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fatma_data_siswa)
+    }
+}
